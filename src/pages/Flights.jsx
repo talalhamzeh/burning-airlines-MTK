ee9954809973eeb67d82683ae088f4199bcdf36f
Michael Anderson <manderson3568@outlook.com.au>
2022-11-16 10:39:03 +1100

    Added multiple page capability

---
+++ src/pages/Flights.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Flights(){
+    return(
+        <h1>Flights</h1>
+    );
+};
+
+export default Flights
---
+++ src/pages/Reservations.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+function Reservations(){
+    <h1>Reservations</h1>
+}
+export default Reservations;
