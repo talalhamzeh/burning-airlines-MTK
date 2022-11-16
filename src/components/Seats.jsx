b7676afef1fb9296ff22fd9ced5c7a61de443a3a
徐敏倩 <josatoshi@josatoshis-MacBook-Pro.local>
2022-11-16 17:01:51 +1100

    reservation update

--- src/components/Seats.jsx
+++ src/components/Seats.jsx
@@ -1,12 +1,56 @@
 import React from 'react'
-
+const seats=[]
 const Seats =(props)=>{
     const display=()=>{
         console.log(props)
     }
     display()
+    const getSeats = ()=>{
+        const column =[ 'A', "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"]
+
+        for (let c = 0; c < props.plane.columns ; c++) {
+            for (let r = 0; r < props.plane.rows ; r++) {
+                seats.push(`${column[c]}${r}`)
+            }
+        }
+        console.log(seats)
+    }
+    getSeats()
+
+
+
+ 
+
+
+
+
+
+
     return(
-        <h1>seats</h1>        
+<div>
+<h1>seats</h1>
+
+<ul>
+
+    <li> {props.flight.date} </li>
+    <li> {props.flight.number} </li>
+    <li> {props.flight.departure} </li>
+    <li> {props.flight.destination} </li>
+  
+
+
+</ul>
+
+{seats.map((s)=> <input type="button" value={s} />)}
+
+
+
+
+
+</div>
+
+
+
     )
 }
 
